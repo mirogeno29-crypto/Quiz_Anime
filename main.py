af03aa5ee0e0a7eaa5818955ba106c2f38cb2435
Gustavo <mirogeno29@gmail.com>
2026-06-18 11:10:20 +0200

    empece a hcer la funcion de igualar nombres y cambie los nombres de los animes a los titulos de estados unidos

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import op_clase
-print("bienvenido al mejor Quiz de la historia del anime: ")
+print("bienvenido al mejor Quiz de la historia de anime: ")
 
 op_clase.op.reproductor_incremento()
--- op_clase.py
+++ op_clase.py
@@ -5,22 +5,24 @@ pygame.mixer.init()
 
 
 class op:
+ op_azar = None
  def seleccion_cancion_azar(): 
   nombre_openings = {
- "Mirai nikki":  "audios anime/Mirai Nikki.mp3",   
-  "Evangelion":   "audios anime/Evangelion.mp3",
-  "jojos" : "audios anime/jojo op2.mp3",
-  "Kimetsu no yaiba" : "audios anime/kimetsu no yaiba.mp3",
-  "Mis kobayashi" : "audios anime/Miss Kobayashi's Dragon Maid.mp3",
-  "My heroe academia" : "audios anime/My heroe academia.mp3",
-  "Nekomonogatari" : "audios anime/Nekomonogatari.mp3",
-  "one piece" : "audios anime/one piece.mp3",
-  "Shingeki no kyojin" : "audios anime/shingeki no kiojin.mp3",
-  "jujutsu" : "audios anime/JUJUTSU KAISEN Opening.mp3",}
+ "Future Diary":  "audios anime/Mirai Nikki.mp3",   
+  "Neon Genesis Evangelion":   "audios anime/Evangelion.mp3",
+  "JoJo's Bizarre Adventure" : "audios anime/jojo op2.mp3",
+  "Demon Slayer" : "audios anime/kimetsu no yaiba.mp3",
+  "Miss Kobayashi's Dragon Maid" : "audios anime/Miss Kobayashi's Dragon Maid.mp3",
+  "My Hero Academia" : "audios anime/My heroe academia.mp3",
+  "Monogatari Series" : "audios anime/Nekomonogatari.mp3",
+  "One Piece" : "audios anime/one piece.mp3",
+  "Attack on Titan" : "audios anime/shingeki no kiojin.mp3",
+  "Jujutsu Kaisen" : "audios anime/JUJUTSU KAISEN Opening.mp3",}
   claves = nombre_openings.keys()
   claves = list(claves)
-  op_azar = random.choice(claves)
-  return nombre_openings[op_azar]
+  op.op_azar = random.choice(claves)
+  
+  return nombre_openings[op.op_azar]
 
  def calcular_duracion_cancion(cancion):
     audio = pygame.mixer.Sound(cancion)
@@ -31,6 +33,7 @@ class op:
  def reproductor_incremento():
     cancion = op.seleccion_cancion_azar()
     duracion = op.calcular_duracion_cancion(cancion)
+    print(op.op_azar)
     aumento = 10
     decremento = 0
     inicio = random.randint(0,duracion-10)
@@ -49,10 +52,15 @@ class op:
        pygame.mixer.music.stop()
       else:
        while True:
+        aumento += 10
         decremento +=10
         print("ejecutar")
         pygame.mixer.music.play(start=inicio-decremento)
         time.sleep(aumento)
         pygame.mixer.music.stop()
+ def  aciertos_op(nombre_usuario,nombre_real):
     
-     
+    
+    
+      
+ 
