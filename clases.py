625d97f2073bd201e0cb34e25189a79d2cac2fb7
Gustavo <mirogeno29@gmail.com>
2026-06-29 10:03:47 +0200

    elimine la version anterior de las clases animes y cree una nuena con instancias bien aplicadas

--- clases.py
+++ clases.py
@@ -1,99 +1,26 @@
 
-class Mirai_Nikki:
-    ruta = "audios anime/Mirai Nikki.mp3"
-    nombre_japones = "Mirai Nikki"
-    nombre_ingles = "Future Diary"
-    nombre_cancion_opening = "Kuusou Mesorogiwi"
-    artista = "Yousei Teikoku"
-    temporada_utilizada = 1
-    numero_opening = 1
+class op_animes():
+    
+    def __init__(self,ruta,nombre_japones,nombre_ingles,nombre_cancion_opening,artista,temporada_utilizo,numero_opening):
+     self.ruta = ruta
+     self.nombre_japones = nombre_japones
+     self.nombre_ingles = nombre_ingles
+     self.nombre_cancion = nombre_cancion_opening
+     self.artista = artista
+     self.temporada_utilizo = temporada_utilizo
+     self.numero_opening = numero_opening
+     
+Mirai_Nikki = op_animes("audios anime/Mirai Nikki.mp3", "Mirai Nikki", "Future Diary", "Kuusou Mesorogiwi", "Yousei Teikoku", 1, 1)
+Shin_Seiki_Evangelion = op_animes("audios anime/Evangelion.mp3", "Shin Seiki Evangelion", "Neon Genesis Evangelion", "A Cruel Angel's Thesis", "Yoko Takahashi", 1, 1)
+JoJo_no_Kimyou_na_Bouken = op_animes("audios anime/jojo op2.mp3", "JoJo no Kimyou na Bouken", "JoJo's Bizarre Adventure", "Bloody Stream", "Coda", 2, 2)
+Kimetsu_no_Yaiba = op_animes("audios anime/kimetsu no yaiba.mp3", "Kimetsu no Yaiba", "Demon Slayer", "Gurenge", "LiSA", 1, 1)
+Kobayashi_san_Chi_no_Maid_Dragon = op_animes("audios anime/Miss Kobayashi's Dragon Maid.mp3", "Kobayashi-san Chi no Maid Dragon", "Miss Kobayashi's Dragon Maid", "Aozora no Rhapsody", "fhána", 1, 1)
+Boku_no_Hero_Academia = op_animes("audios anime/My heroe academia.mp3", "Boku no Hero Academia", "My Hero Academia", "The Day", "Porno Graffitti", 1, 1)
+Monogatari = op_animes("audios anime/Nekomonogatari.mp3", "Monogatari", "Monogatari Series", "Perfect Slumbers", "Yui Horie", 3, 10)
+Wan_Piisu = op_animes("audios anime/one piece.mp3", "Wan Piisu", "One Piece", "We Are!", "Hiroshi Kitadani", 1, 1)
+Shingeki_no_Kyojin = op_animes("audios anime/shingeki no kiojin.mp3", "Shingeki no Kyojin", "Attack on Titan", "Guren no Yumiya", "Linked Horizon", 1, 1)
+Jujutsu_Kaisen = op_animes("audios anime/JUJUTSU KAISEN Opening.mp3", "Jujutsu Kaisen", "Jujutsu Kaisen", "Kaikai Kitan", "Eve", 1, 1)
 
 
-class Shin_Seiki_Evangelion:
-    ruta = "audios anime/Evangelion.mp3"
-    nombre_japones = "Shin Seiki Evangelion"
-    nombre_ingles = "Neon Genesis Evangelion"
-    nombre_cancion_opening = "A Cruel Angel's Thesis"
-    artista = "Yoko Takahashi"
-    temporada_utilizada = 1
-    numero_opening = 1
 
 
-class JoJo_no_Kimyou_na_Bouken:
-    ruta = "audios anime/jojo op2.mp3"
-    nombre_japones = "JoJo no Kimyou na Bouken"
-    nombre_ingles = "JoJo's Bizarre Adventure"
-    nombre_cancion_opening = "Bloody Stream"
-    artista = "Coda"
-    temporada_utilizada = 2
-    numero_opening = 2
-
-
-class Kimetsu_no_Yaiba:
-    ruta = "audios anime/kimetsu no yaiba.mp3"
-    nombre_japones = "Kimetsu no Yaiba"
-    nombre_ingles = "Demon Slayer"
-    nombre_cancion_opening = "Gurenge"
-    artista = "LiSA"
-    temporada_utilizada = 1
-    numero_opening = 1
-
-
-class Kobayashi_san_Chi_no_Maid_Dragon:
-    ruta = "audios anime/Miss Kobayashi's Dragon Maid.mp3"
-    nombre_japones = "Kobayashi-san Chi no Maid Dragon"
-    nombre_ingles = "Miss Kobayashi's Dragon Maid"
-    nombre_cancion_opening = "Aozora no Rhapsody"
-    artista = "fhána"
-    temporada_utilizada = 1
-    numero_opening = 1
-
-
-class Boku_no_Hero_Academia:
-    ruta = "audios anime/My heroe academia.mp3"
-    nombre_japones = "Boku no Hero Academia"
-    nombre_ingles = "My Hero Academia"
-    nombre_cancion_opening = "The Day"
-    artista = "Porno Graffitti"
-    temporada_utilizada = 1
-    numero_opening = 1
-
-
-class monogatari:
-    ruta = "audios anime/Nekomonogatari.mp3"
-    nombre_japones = "Monogatari"
-    nombre_ingles = "Monogatari Series"
-    nombre_cancion_opening = "Perfect Slumbers"
-    artista = "Yui Horie"
-    temporada_utilizada = 3
-    numero_opening = 10
-
-
-class Wan_Piisu:
-    ruta = "audios anime/one piece.mp3"
-    nombre_japones = "Wan Piisu"
-    nombre_ingles = "One Piece"
-    nombre_cancion_opening = "We Are!"
-    artista = "Hiroshi Kitadani"
-    temporada_utilizada = 1
-    numero_opening = 1
-
-
-class Shingeki_no_Kyojin:
-    ruta = "audios anime/shingeki no kiojin.mp3"
-    nombre_japones = "shingeki no kyojin"
-    nombre_ingles = "Attack on Titan"
-    nombre_cancion_opening = "Guren no Yumiya"
-    artista = "Linked Horizon"
-    temporada_utilizada = 1
-    numero_opening = 1
-
-
-class Jujutsu_Kaisen:
-    ruta = "audios anime/JUJUTSU KAISEN Opening.mp3"
-    nombre_japones = "Jujutsu Kaisen"
-    nombre_ingles = "Jujutsu Kaisen"
-    nombre_cancion_opening = "Kaikai Kitan"
-    artista = "Eve"
-    temporada_utilizada = 1
-    numero_opening = 1
